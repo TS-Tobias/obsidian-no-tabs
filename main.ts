92e2aa36703b9cc19b61698f452890dbe2d5ee72
Tobias Schuster <schuster.tobias@gmx.net>
2023-04-23 20:52:13 +0200

    Improvements to the previous implementation. The plugin is no longer relying on a timer to detach the previous tab. Added a 'strict mode' that will only allow one tab (in addition to pinned tabs). The new mode can be enabled in the plugin's new settings tab

--- main.ts
+++ main.ts
@@ -1,42 +1,87 @@
-import { Plugin, TFile, WorkspaceLeaf } from "obsidian";
+import { Plugin, WorkspaceLeaf } from "obsidian";
+import SettingsTab from "settings";
 
-/**
-When a new note is created in the vault, the most recent leaf is detached from the workspace if it is not pinned.
+interface Data {
+	settings: Settings;
+}
 
+interface Settings {
+	strictMode: boolean;
+}
 
-Here is a brief overview of the things that happen when a new note is created:
-1. New file f is created (Obsidian)
-2. Temporarily store current leaf x (Extension)
-3. Create new leaf y and open file f inside it (Obsidian)
-4. Set new leaf to active (Obsidian)
-5. Detach previous leaf x (Extension)
+const DEFAULT_SETTINGS: Settings = {
+	strictMode: false,
+};
 
-Because `getMostRecentLeaf()` gets updated between the `app.vault create` and `app.workspace file-open` event to the new active leaf, the previous leaf has to be stored inside the `lastOpened` variable.
+const DEFAULT_DATA: Partial<Data> = {
+	settings: DEFAULT_SETTINGS,
+};
 
-Calling `detach()` inside the `create` event handler causes issues with split views in Obsidian. If the previous tab is detached before the new tab is created (file-open), the split-view will close if there is only one tab left (and the new tab will be created in the preceding (split-)view).
- */
 export default class NoTabsPlugin extends Plugin {
-	lastOpened: WorkspaceLeaf | null = null;
+	data: Data;
+	activeTab: WorkspaceLeaf | null = null;
+	newFile = false;
 
 	async onload() {
+		await this.loadData();
+
 		this.registerEvent(
 			this.app.vault.on("create", () => {
-				this.lastOpened = this.app.workspace.getMostRecentLeaf();
+				if (this.data.settings.strictMode) return;
+
+				this.activeTab = this.app.workspace.getMostRecentLeaf();
+				if (!this.activeTab) return;
+				const { pinned, type } = this.activeTab.getViewState();
+
+				if (pinned) return;
+				if (type === "empty") return;
+
+				this.newFile = true;
 			})
 		);
+
 		this.registerEvent(
-			this.app.workspace.on("file-open", (file: TFile) => {
-				const timeSinceCreation = Date.now() - file.stat.ctime;
-				const leaf = this.app.workspace.getMostRecentLeaf();
-				if (
-					this.lastOpened &&
-					this.lastOpened !== leaf &&
-					!this.lastOpened.getViewState().pinned &&
-					timeSinceCreation < 1000
-				) {
-					this.lastOpened.detach();
-				}
+			this.app.workspace.on("file-open", () => {
+				if (this.data.settings.strictMode) return;
+				if (!this.newFile) return;
+				this.newFile = false;
+				this.activeTab?.detach();
+				this.activeTab = this.app.workspace.getMostRecentLeaf();
 			})
 		);
+
+		this.registerEvent(
+			this.app.workspace.on(
+				"active-leaf-change",
+				(activeLeaf: WorkspaceLeaf) => {
+					if (!this.data.settings.strictMode) return;
+					const { type } = activeLeaf.getViewState();
+					if (type === "empty" || type === "markdown") {
+						// @ts-ignore
+						activeLeaf.parent.children.forEach(
+							(leaf: WorkspaceLeaf) => {
+								if (
+									// @ts-ignore
+									leaf.id !== activeLeaf.id &&
+									!leaf.getViewState().pinned
+								) {
+									leaf.detach();
+								}
+							}
+						);
+					}
+				}
+			)
+		);
+
+		this.addSettingTab(new SettingsTab(this.app, this));
+	}
+
+	async saveData(): Promise<void> {
+		await super.saveData(this.data);
+	}
+
+	async loadData(): Promise<void> {
+		this.data = Object.assign({}, DEFAULT_DATA, await super.loadData());
 	}
 }
---
+++ settings.ts
@@ -0,0 +1,30 @@
+import NoTabsPlugin from "main";
+import { App, PluginSettingTab, Setting } from "obsidian";
+
+export default class SettingsTab extends PluginSettingTab {
+	private readonly plugin: NoTabsPlugin;
+
+	constructor(app: App, plugin: NoTabsPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+		containerEl.empty();
+		containerEl.createEl("h2", { text: "NoTabs Settings" });
+		new Setting(containerEl)
+			.setName("Strict Mode")
+			.setDesc(
+				"Warning: Strict Mode will close all tabs that are not active or  pinned"
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.data.settings.strictMode)
+					.onChange(async (value: boolean) => {
+						this.plugin.data.settings.strictMode = value;
+						await this.plugin.saveData();
+					})
+			);
+	}
+}
